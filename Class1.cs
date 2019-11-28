6041cfff00fd692b33ee800ac6b56ad512cfb062
luishenriquegj <45577217+luishenriquegj@users.noreply.github.com>
2019-11-27 22:47:25 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+    void MoveCharacter()
+    {
+        PlayerRigid.MovePosition(transform.position + (change.normalized * speed * Time.deltaTime));
+    }
+}
